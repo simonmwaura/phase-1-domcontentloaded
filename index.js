a14d6cc1296343d30dc35708a319dc43c0ae8440
simonmwaura <simonmwaura2090@gmail.com>
2024-04-04 04:28:46 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    updateTheDOM()
+  });
+
+  function updateTheDOM() {
+    document.getElementById( "text" )
+      .innerHTML = "This is really cool!";
+  }
